e0be0ef3de8dfb5945de13e3ae083708c138f426
Lucas Turpin <lucas_turpin@hotmail.com>
2019-01-20 06:20:20 -0500

    Refactor Map into component

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,18 +1,12 @@
-import React, { Component } from 'react';
-import './App.css';
-import Map from './MapContainer'
+import React, { Component } from "react";
+import "./App.css";
+import Map from "./MapContainer";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Map 
-          initialCenter={{
-            lat: 45.50884,
-            lng: -73.58781
-          }}
-          zoom={12}
-        ></Map>
+        <Map />
       </div>
     );
   }
--- ui/src/MapContainer/index.jsx
+++ ui/src/MapContainer/index.jsx
@@ -1,16 +1,30 @@
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, HeatmapLayer } from "google-maps-react";
+import React, { Component } from "react";
 
-
-class MapContainer extends Map {
-    componentDidMount() {
-        // Get points from backend
-        // Create Heatmap objects with reference to this.props.map
-        // Config heatmaps
-        super.componentDidMount()
-        console.log('Hello world')
-        const { map } = this.props;
-    }
-    
+class MapContainer extends Component {
+  state = {
+    layers: []
+  };
+  componentDidMount() {
+    // Request points from backend
+    console.log('Test')
+  };
+  render() {
+    return (
+      <Map
+        initialCenter={{
+          lat: 45.50884,
+          lng: -73.58781
+        }}
+        zoom={12}
+        {...this.props}
+      >
+        {this.state.layers}
+      </Map>
+    );
+  }
 }
 
-export default GoogleApiWrapper({apiKey: process.env.REACT_APP_API_KEY})(MapContainer);
+export default GoogleApiWrapper({ apiKey: process.env.REACT_APP_API_KEY })(
+  MapContainer
+);
