51098b05af964bffbcd35c4cdbeb70dd72ab5233
Chirac <chiracmanoukian@hotmail.com>
2019-01-20 02:02:09 -0500

    Connection to db

--- server/app.js
+++ server/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+// require('dotenv').config();
 
 const PORT = 8080;
 const HOST = '0.0.0.0';
+
 const app = express();
 
+mongoose.connect('mongodb://mongo:27017');
+
 app.get('/', (req, res) => {
-    res.send('Hi bud');
+    res.send('boom');
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
