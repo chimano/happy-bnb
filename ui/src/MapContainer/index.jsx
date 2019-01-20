fd361f332d3bb667ce08bd454c2b9a855aecff3c
Lucas Turpin <lucas_turpin@hotmail.com>
2019-01-20 07:54:53 -0500

    Temp maps integration

--- ui/src/MapContainer/index.jsx
+++ ui/src/MapContainer/index.jsx
@@ -1,14 +1,40 @@
-import { Map, GoogleApiWrapper, HeatmapLayer } from "google-maps-react";
+import { Map, GoogleApiWrapper } from "google-maps-react";
+import "google-maps-react";
 import React, { Component } from "react";
+import { getRentals } from "../utils/httpRequests";
 
 class MapContainer extends Component {
   state = {
-    layers: []
+    high_ratings: [],
+    low_ratings: []
   };
   componentDidMount() {
     // Request points from backend
-    console.log('Test')
-  };
+    getRentals("high")
+      .then(resp => {
+        this.setState(
+          {
+            high_ratings: resp.data.map(
+              item =>
+                new this.props.google.maps.LatLng(item.latitude, item.longitude)
+            )
+          },
+          () => {
+            console.log(this.state.high_ratings);
+          }
+        );
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    getRentals("low")
+      .then(resp => {
+        this.setState({ high_ratings: resp.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
   render() {
     return (
       <Map
@@ -19,7 +45,26 @@ class MapContainer extends Component {
         zoom={12}
         {...this.props}
       >
-        {this.state.layers}
+      {/* <this.props.google.maps.HeatmapLayer
+            data={[
+                new this.props.google.maps.LatLng(37.782551, -122.445368),
+                new this.props.google.maps.LatLng(37.782745, -122.444586),
+                new this.props.google.maps.LatLng(37.782842, -122.443688),
+                new this.props.google.maps.LatLng(37.782919, -122.442815),
+                new this.props.google.maps.LatLng(37.782992, -122.442112),
+                new this.props.google.maps.LatLng(37.783100, -122.441461)
+            ]}
+        /> */}
+        {/* {this.state.high_ratings.length ? (
+          <HeatmapLayer data={this.state.high_ratings} />
+        ) : (
+          ""
+        )}
+        {this.state.low_ratings.length ? (
+          <HeatmapLayer data={this.state.low_ratings} />
+        ) : (
+          ""
+        )} */}
       </Map>
     );
   }
